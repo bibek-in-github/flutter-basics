a4a90306a9d9e290f84e140fbb86a7cb82bea2a5
bibek-in-github <mahatcoding@gmail.com>
2023-08-05 21:21:58 +0545

    feat: variables in flutter

---
+++ variable.dart
@@ -0,0 +1,17 @@
+void main()
+{
+
+	int number = 10;
+
+	double number2 = 0.2;
+	bool myBool = false; 
+
+	String myString = "0", myString2 = "i wanna live ";
+
+	
+	print(number); 
+	print(number2); 
+	print(myBool); 
+	print(myString); 
+	print(myString2); 
+}
