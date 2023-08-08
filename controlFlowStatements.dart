80aa6ddc6c2a205f24649623f13d385d982ab5e9
bibek-in-github <mahatcoding@gmail.com>
2023-08-08 16:53:07 +0545

    fix:refactor controlFlowStatements.dart

--- controlFlowStatements.dart
+++ controlFlowStatements.dart
@@ -1,10 +1,9 @@
-void main()
-{
-    var num = 75;
-    print("Wallah - Dart If Statement");
-  	
-  	if(num > 50){
-      print("Number is Greater than 50");
-    }
-  
+//dart if statement
+void main() {
+  var num = 75;
+  print("Wallah - Dart If Statement");
+
+  if (num > 50) {
+    print("Number is Greater than 50");
+  }
 }
